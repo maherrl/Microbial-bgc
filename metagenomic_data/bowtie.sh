1f6ecebe4b9422c7c30e47251e5b4fffacaab016
Becca Maher <maherr@oregonstate.edu>
2022-02-14 14:32:33 -0800

    Update bowtie.sh
    Updated to include a for loop for all samples and all replicates of each sample.

--- metagenomic_data/bowtie.sh
+++ metagenomic_data/bowtie.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 #SBATCH --account=crobe
-#SBATCH --partition=fat
+#SBATCH --partition=longfat
 #SBATCH --job-name=bwt
 #SBATCH --output=bwt.out
 #SBATCH --error=bwt.err
-#SBATCH --time=1000
-#SBATCH --mem=25000M
+#SBATCH --time=10000
+#SBATCH --mem=500000M
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=15
 
+NUM_THREADS=15
 base="/home/rmaher2/crobe/neon/2019"
-in="${base}/anvio/contigs"
-reads="${base}/clean"
-out="${base}/anvio/mapping/BLDE"
+in="${base}/clean"
+contigs="${base}/assembly"
+out="${base}/mapping"
 
 module load bowtie2/2.4.4
-module load samtools/1.14
 
-#bowtie2-build ${in}/BLDE_spades_co.fasta ${in}/bwt_dbs/BLDE_spades_co.db --threads 15
-
-for sample in `awk '{print $1}' ${reads}/BLDE.samples.txt`
+for sample in `awk '{print $1}' ${in}/samples.txt`
 do
     if [ "$sample" == "sample" ]; then continue; fi
-    bowtie2 --threads 15 \
-    -x ${in}/bwt_dbs/BLDE_spades_co.db \
-    -1 ${reads}/${sample}_R1.fastq \
-    -2 ${reads}/${sample}_R2.fastq \
-    --no-unal \
-    -S ${out}/${sample}.sam
     
-    samtools view -F 4 -bS ${out}/${sample}.sam > ${out}/${sample}-RAW.bam
-    anvi-init-bam ${out}/${sample}-RAW.bam -o ${out}/${sample}.bam
-
+    # you need to make sure you "ls 01_QC/*QUALITY_PASSED_R1*" returns R1 files for all your samples in samples.txt
+    R1s=`ls ${in}/$sample*.DNA-DNA1_clean_R1.fastq | python -c 'import sys; print(",".join([x.strip() for x in sys.stdin.readlines()]))'`
+    R2s=`ls ${in}/$sample*.DNA-DNA1_clean_R2.fastq | python -c 'import sys; print(",".join([x.strip() for x in sys.stdin.readlines()]))'`
+    
+    bowtie2-build ${contigs}/$sample/contigs.fasta ${contigs}/$sample/contigs.db --threads $NUM_THREADS
+    bowtie2 --threads $NUM_THREADS -x ${contigs}/$sample/contigs.db -1 $R1s -2 $R2s --no-unal -S ${out}/$sample.sam
+    samtools view -F 4 -bS ${out}/$sample.sam > ${out}/$sample-RAW.bam
+    anvi-init-bam ${out}/$sample-RAW.bam -o ${out}/$sample.bam
 done
