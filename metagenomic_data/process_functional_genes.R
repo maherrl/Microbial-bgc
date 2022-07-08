98cb2795a32b9203c6206f7358549d40cd4a59ac
Becca Maher <29050285+maherrl@users.noreply.github.com>
2022-07-08 13:33:45 -0700

    Adding transcripts per million normalization

--- metagenomic_data/process_functional_genes.R
+++ metagenomic_data/process_functional_genes.R
@@ -11,9 +11,9 @@ library("reshape")
 
 swap_accession_for_key <- function(coverage, functions) {
   functions %>% 
-    select(key = gene_callers_id, accession, `function`) %>% 
-    left_join(coverage, by = "key") %>% 
-    select(-key) %>% 
+    select(gene_callers_id, accession, `function`) %>% 
+    left_join(coverage, by = "gene_callers_id") %>% 
+    select(-gene_callers_id) %>% 
     mutate(`function` = word(`function`, sep = "!!!")) %>% 
     mutate(accession = word(accession, sep = "!!!")) %>% 
     drop_na()%>%
@@ -24,17 +24,16 @@ swap_accession_for_key <- function(coverage, functions) {
 
 #####
 ## Analysis
-setwd("/gpfs/home/rmaher2/crobe/neon/surface/genes")
+setwd("/gpfs/home/rmaher2/crobe/neon")
 
-sample_names <- read.table(file = "../databases/names.txt")
-x <- sample_names$V1
+x <- list.files("./profiles")
 
 lsTab <- list()
 for(i in x) {
   
-  cov <- read_table(file = paste0(i,"_gene_table-GENE-COVERAGES.txt"), col_types = cols())
-# cov[-1] <- sapply(cov[-1], function(x) {as.numeric(x > 0)}) # option for getting binary table for counts 
-  func <- read_tsv(file = paste0(i,"_functions.txt"), col_types = cols()) %>%
+  cov <- read_table(file = paste0("./gene_coverages/",i,"-gene_coverages.txt"), col_types = cols())
+  cov[-1] <- sapply(cov[-1], function(x) {x/(sum(x)/1e6)}) # transcripts per million transformation
+  func <- read_tsv(file = paste0("./functions/",i,"_functions.txt"), col_types = cols()) %>%
     filter(source == "Pfam") %>% 
     select(-e_value)
   mat <- swap_accession_for_key(cov, func)
@@ -44,4 +43,6 @@ for(i in x) {
 
 tab_all <- merge_recurse(lsTab, by = c("accession", "function"))
 
-write.csv(tab_all, file = "./Surface_gene_by_sample_Pfam_coverages.csv", row.names = FALSE)
+write.csv(tab_all, file = "./results/gene_tab_Pfam.csv", row.names = FALSE)
+
+colnames(tab_all)[-2]
